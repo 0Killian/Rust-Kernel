ef1dbfd0617a8e79da735e65484bbe5f5652f88b
0Killian <killianbellouard@gmail.com>
2022-08-22 02:29:07 +0200

    Added serial printing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod serial;
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> !
 {
@@ -15,6 +17,8 @@ fn kernel_main(boot_info : &'static mut bootloader::BootInfo) -> !
     {
         framebuffer.buffer_mut().fill(0x90);
     }
+
+    serial_println!("Hello from kernel!");
 
     loop {}
 }
